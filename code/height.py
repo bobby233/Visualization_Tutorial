ddb357b734da76c4faa007e86781173f68f14086
bobby233 <mczsjzsjz@outlook.com>
2020-03-08 20:29:28 +0800

    今天写这么多了，累了

--- code/height.py
+++ code/height.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 ages = [10, 11, 12, 13]
 heights = [170, 156, 189, 175]
-plt.scatter(ages, heights)
+plt.scatter(ages, heights, s=300, c='red')
 
 plt.title('Heights of Different Ages', fontsize=30)
 plt.xlabel('Ages', fontsize=17)
